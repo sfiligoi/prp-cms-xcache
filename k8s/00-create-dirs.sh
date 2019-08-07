dce77135f55cd0aabbb66685443662a704c12247
efajardo <efajardo@physics.ucsd.edu>
2019-08-07 13:39:38 -0700

    Making it work with new docker image and RPM

--- k8s/00-create-dirs.sh
+++ k8s/00-create-dirs.sh
@@ -7,6 +7,7 @@ for i in ${dirNums[@]}; do
     mkdir /data${i}
   fi
   if [ ! -d "/data${i}/xcache" ]; then
-    mkdir /data${i}/xcache && chown xrootd:xrootd /data${i}/xcache
+    mkdir -p /data${i}/xcache && chown xrootd:xrootd /data${i}/xcache
   fi
 done
+chown xrootd:xrootd /xcache-root
